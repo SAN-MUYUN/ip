bd63a46a0694ab2b6701fc44d021431022e0ebc9
SAN-MUYUN <E1121291@U.NUS.EDU>
2025-01-23 01:09:42 +0800

    Add echo

--- src/main/java/MuyunBot.java
+++ src/main/java/MuyunBot.java
@@ -1,15 +1,48 @@
+import java.util.Scanner;
+
 public class MuyunBot {
-    public static void main(String[] args) {
-        String greeting = PrintStyle.dashedLines()
-                + "\n"
-                + "Hello! I'm MuyunBot \n"
-                + "What can I do for you? \n"
-                + "\n"
-                + PrintStyle.dashedLines()
-                + "\n"
-                + "Bye! Hope to see you soon! \n"
+
+    private static void greet() {
+//        display greeting when starting the bot;
+
+        String text = PrintStyle.dashedLines()
+                + PrintStyle.indent("Hello! I'm MuyunBot")
+                + PrintStyle.indent("What can I do for you?")
                 + PrintStyle.dashedLines();
-        System.out.println(greeting);
+        System.out.println(text);
+    }
+
+    private static void quit() {
+//        display ending message when closing the bot;
+        String text = PrintStyle.dashedLines()
+                + PrintStyle.indent("Bye! Hope to see you soon!")
+                + PrintStyle.dashedLines();
+        System.out.println(text);
+    }
+
+    private static void echo(String input) {
+        display(input);
+    }
+
+    private static void display(String x) {
+//        display x in proper style;
+        String text = PrintStyle.dashedLines()
+                + PrintStyle.indent(x)
+                + PrintStyle.dashedLines();
+        System.out.println(text);
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        greet();
+        String input = scanner.nextLine();
+//        continue echoing until user inputs "bye";
+        while(!input.equals("bye")) {
+            echo(input);
+            input = scanner.nextLine();
+        }
+        quit();
+
     }
 
 }
--- src/main/java/PrintStyle.java
+++ src/main/java/PrintStyle.java
@@ -1,6 +1,11 @@
 
 public class PrintStyle {
     public static String dashedLines() {
-        return ("----------");
+        return ("    __________\n");
+    }
+
+    public static String indent(String x) {
+//        create indentation on x for display be the bot;
+        return ("    " + x + "\n");
     }
 }
